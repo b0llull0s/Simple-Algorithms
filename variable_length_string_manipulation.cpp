0a334a3746a82512b54412923592f2d6825e1a2e
b0llull0s <panicthreads@proton.me>
2025-01-17 13:55:40 +0000

    17-01-25

--- variable_length_string_manipulation.cpp
+++ variable_length_string_manipulation.cpp
@@ -15,9 +15,18 @@ char characterAT(arrayString s, int position) {
     return s[position];
 }
 
-void append(arrayString&  s, xhsr c) {
-
+void append(arrayString& s, char c) {
+    int oldLength = length(s);
+    arrayString newS = new char[oldLength + 2];
+    for (int i = 0; i < oldLength; i++) {
+        newS[i] = s[i];
+    }
+    newS[oldLength] = c;
+    newS[oldLength + 1] = 0;
+    delete[] s;
+    s = newS; 
 }
+
 void appendTester() {
     arrayString a = new char[5];
     a[0] = 't'; a[1] = 'e'; a[2] = 's'; a[3] = 't'; a[4] = 0;
@@ -57,18 +66,6 @@ int length(arrayString s) {
     }
     return count;
 }
-void append(arrayString& s, char c) {
-    int oldLength = length(s);
-    arrayString newS = new char[oldLength + 2];
-    for (int i = 0; i < oldLength; i++) {
-        newS[i] = s[i];
-    }
-    newS[oldLength] = c;
-    newS[oldLength + 1] = 0;
-    delete[] s;
-    s = newS; 
-}
-
 
 void Concatenate(arrayString& s1, arrayString s2) {   
 }
