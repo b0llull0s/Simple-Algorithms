d04869ab023474979e86818b443b10820746ac71
b0llull0s <panicthreads@proton.me>
2025-01-18 14:56:35 +0000

    18-01-2025

---
+++ exercise-4-2.cpp
@@ -0,0 +1,10 @@
+/*
+Exercise 4-2:
+For our dynamically allocated strings, create a function substring that takes three parameters:
+   - An arrayString.
+   - A starting position integer.
+   - An integer length of characters.
+The function returns a pointer to a new dynamically allocated string array.
+This string array contains the characters in the original string, starting at the specified position for the specified length.
+The original string is unaffected by the operation. So if the original string was abcdefg, the position was 3, and the length was 4, then the new string would contain cdef.
+*/
