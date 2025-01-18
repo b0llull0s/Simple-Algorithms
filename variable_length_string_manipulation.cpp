21c9fe0453ecc3ac58edcd237945018e816f0dcf
b0llull0s <panicthreads@proton.me>
2025-01-18 20:03:52 +0000

    18-01-2025

--- variable_length_string_manipulation.cpp
+++ variable_length_string_manipulation.cpp
@@ -20,143 +20,91 @@ Exercise 4-3:
 For our dynamically allocated strings, create a function replaceString that takes three parameters, each of type arrayString: source, target, and replaceText.
 The function replaces every occurrence of target in source with replaceText.
 For example, if source points to an array containing abcdabee, target points to ab, and replacetext points to xyz, then when the function ends, source should point to an array containing xyzcdxyzee
+
+Exercise 4-4:
+Change the implementation of our strings such that location[0] in the array stores the size of the array(and therefore location[1] stores the first actual character in the string) rather than using a null-character terminator.
+Implement each of the three functions, append, concatenate, and characterAt, taking advantage of the stored size information whenever possible.
+Because we'll no longer using the null-termination convention expected by the standard output stream, you'll need to write your own output function that loops through its string parameter, displaying characters.
 */
 #include <iostream>
 using namespace std;
 
-typedef char * arrayString;
+typedef char* arrayString;
 
 char characterAT(arrayString s, int position) {
-    return s[position];
+    return s[position + 1]; // Adjust for size stored at s[0]
 }
 
 void append(arrayString& s, char c) {
-    int oldLength = 0;
-    while (s[oldLength] != 0) {
-        oldLength++;
-    }
-    arrayString newS = new char[oldLength + 2];
-    for (int i = 0; i < oldLength; i++) {
+    int oldLength = s[0]; // Get the old length from s[0]
+    arrayString newS = new char[oldLength + 2]; // Allocate new array with extra space
+    newS[0] = oldLength + 1; // Update the size
+    for (int i = 1; i <= oldLength; i++) {
         newS[i] = s[i];
     }
-    newS[oldLength] = c;
-    newS[oldLength + 1] = 0;
+    newS[oldLength + 1] = c; // Append the new character
     delete[] s;
     s = newS;
 }
 
 void appendTester() {
     arrayString a = new char[5];
-    a[0] = 't'; a[1] = 'e'; a[2] = 's'; a[3] = 't'; a[4] = 0;
+    a[0] = 4; // Size
+    a[1] = 't'; a[2] = 'e'; a[3] = 's'; a[4] = 't';
     append(a, '!');
-    cout << a << "\n";
+    for (int i = 1; i <= a[0]; i++) {
+        cout << a[i];
+    }
+    cout << "\n";
 }
 
 int length(arrayString s) {
-    int count = 0;
-    while (s[count] != 0) {
-        count++;
-    }
-    return count;
+    return s[0]; // Return the length stored at s[0]
 }
 
 void Concatenate(arrayString& s1, arrayString s2) {
-    int s1_OldLength = length(s1);
-    int s2_Length = length(s2);
+    int s1_OldLength = s1[0];
+    int s2_Length = s2[0];
     int s1_NewLength = s1_OldLength + s2_Length;
     arrayString newS = new char[s1_NewLength + 1];
-    for(int i = 0; i < s1_OldLength; i++) {
+    newS[0] = s1_NewLength; // Update the size
+    for (int i = 1; i <= s1_OldLength; i++) {
         newS[i] = s1[i];
     }
-    for(int i = 0; i < s2_Length; i++) {
+    for (int i = 1; i <= s2_Length; i++) {
         newS[s1_OldLength + i] = s2[i];
     }
-    newS[s1_NewLength] = 0;
     delete[] s1;
     s1 = newS;
 }
 
 void ConcatenateTester() {
     arrayString a = new char[5];
-    a[0] = 't'; a[1] = 'e'; a[2] = 's'; a[3] = 't'; a[4] = 0;
+    a[0] = 4; // Size
+    a[1] = 't'; a[2] = 'e'; a[3] = 's'; a[4] = 't';
     arrayString b = new char[4];
-    b[0] = 'b'; b[1] = 'e'; b[2] = 'd'; b[3] = 0;
+    b[0] = 3; // Size
+    b[1] = 'b'; b[2] = 'e'; b[3] = 'd';
     Concatenate(a, b);
-    cout << a << "\n" << b << "\n";
-}
-
-arrayString substring(arrayString s, int start, int length) {
-    arrayString sub = new char[length + 1];
-    for (int i = 0; i < length; i++) {
-        sub[i] = s[start + i];
+    for (int i = 1; i <= a[0]; i++) {
+        cout << a[i];
     }
-    sub[length] = 0;
-    return sub;
-}
-
-void substringTester() {
-    arrayString a = new char[8];
-    a[0] = 'a'; a[1] = 'b'; a[2] = 'c'; a[3] = 'd'; a[4] = 'e'; a[5] = 'f'; a[6] = 'g'; a[7] = 0;
-    arrayString sub = substring(a, 2, 4);
-    cout << sub << "\n";
-    delete[] sub;
-}
-
-void replaceString(arrayString& source, arrayString target, arrayString replaceText) {
-    int sourceLength = length(source);
-    int targetLength = length(target);
-    int replaceTextLength = length(replaceText);
-
-    // Count occurrences of target in source
-    int count = 0;
-    for (int i = 0; i < sourceLength; i++) {
-        if (strncmp(&source[i], target, targetLength) == 0) {
-            count++;
-            i += targetLength - 1;
-        }
+    cout << "\n";
+    for (int i = 1; i <= b[0]; i++) {
+        cout << b[i];
     }
-
-    // Calculate new length for the source after replacements
-    int newLength = sourceLength + count * (replaceTextLength - targetLength);
-    arrayString newSource = new char[newLength + 1];
-
-    // Replace occurrences of target with replaceText
-    int index = 0;
-    for (int i = 0; i < sourceLength; i++) {
-        if (strncmp(&source[i], target, targetLength) == 0) {
-            for (int j = 0; j < replaceTextLength; j++) {
-                newSource[index++] = replaceText[j];
-            }
-            i += targetLength - 1;
-        } else {
-            newSource[index++] = source[i];
-        }
-    }
-    newSource[newLength] = 0;
-
-    // Update source
-    delete[] source;
-    source = newSource;
+    cout << "\n";
 }
 
-void replaceStringTester() {
-    arrayString a = new char[9];
-    a[0] = 'a'; a[1] = 'b'; a[2] = 'c'; a[3] = 'd'; a[4] = 'a'; a[5] = 'b'; a[6] = 'e'; a[7] = 'e'; a[8] = 0;
-    arrayString target = new char[3];
-    target[0] = 'a'; target[1] = 'b'; target[2] = 0;
-    arrayString replaceText = new char[4];
-    replaceText[0] = 'x'; replaceText[1] = 'y'; replaceText[2] = 'z'; replaceText[3] = 0;
-    replaceString(a, target, replaceText);
-    cout << a << "\n";
-    delete[] a;
-    delete[] target;
-    delete[] replaceText;
+void customOutput(arrayString s) {
+    for (int i = 1; i <= s[0]; i++) {
+        cout << s[i];
+    }
+    cout << "\n";
 }
 
 int main() {
     appendTester();
     ConcatenateTester();
-    substringTester();
-    replaceStringTester();
     return 0;
 }
