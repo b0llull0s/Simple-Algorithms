72f43278b2b502ce8e6f843f84fadaa257f9485d
b0llull0s <panicthreads@proton.me>
2025-01-20 18:58:13 +0000

    20-01-2025

--- LinkedListNumber.cpp
+++ LinkedListNumber.cpp
@@ -4,8 +4,10 @@ Imagine a linked list where instead of the node storing a character, the node st
 We could represent positive numbers of any size using such a linked list; the number 149, for example, would be a linked list in which the node stores a 1, the second a 4, and the third and last a 9.
 Write a function intToList that takes an integer value and produces a linked list of this sort.
 Hint: You may find it easier to build the linked list backward, so if the value were 149, you would create the 9 node first.
-*/
 
+Exercise 4-10:
+For the digit list of the previous exercise, write a function that takes two such lists and produces a new list representing their sum.
+*/
 #include <iostream>
 using namespace std;
 
@@ -47,7 +49,7 @@ public:
         cout << endl;
     }
 
-    int length() const {
+    int length() const {  // Constant time O(1) access to the size, since it just returns the tracked size member.
         return size;
     }
 
@@ -59,10 +61,37 @@ public:
         }
         while (value > 0) {
             int digit = value % 10;
-            list.append(digit);
+            list.prepend(digit);
             value /= 10;
         }
-        return list.reverse();
+        return list;
+    }
+
+    static LinkedListNumber sumLists(const LinkedListNumber& l1, const LinkedListNumber& l2) {
+        // First reverse both input lists
+        LinkedListNumber rev1 = l1.reverse();
+        LinkedListNumber rev2 = l2.reverse();
+        
+        LinkedListNumber result;
+        ListNode* p1 = rev1.head;
+        ListNode* p2 = rev2.head;
+        int carry = 0;
+        
+        while (p1 != nullptr || p2 != nullptr || carry != 0) {
+            int sum = carry;
+            if (p1 != nullptr) {
+                sum += p1->data;
+                p1 = p1->next;
+            }
+            if (p2 != nullptr) {
+                sum += p2->data;
+                p2 = p2->next;
+            }
+            carry = sum / 10;
+            result.append(sum % 10);
+        }
+        
+        return result.reverse();
     }
 
 private:
@@ -83,11 +112,31 @@ private:
         }
         return reversedList;
     }
+
+    void prepend(int digit) {
+        ListNode* newNode = new ListNode{digit, head};
+        head = newNode;
+        if (tail == nullptr) {
+            tail = newNode;
+        }
+        size++;
+    }
 };
 
 void intToListTester() {
-    LinkedListNumber list = LinkedListNumber::intToList(149);
-    list.display();  // Should output "149"
+    LinkedListNumber list1 = LinkedListNumber::intToList(149);
+    LinkedListNumber list2 = LinkedListNumber::intToList(1450);
+    
+    cout << "First linked list: ";
+    list1.display();
+    
+    cout << "Second linked list: ";
+    list2.display();
+    
+    LinkedListNumber sumList = LinkedListNumber::sumLists(list1, list2);
+    
+    cout << "Sum of linked lists: ";
+    sumList.display();  // Should output "1599"
 }
 
 int main() {
